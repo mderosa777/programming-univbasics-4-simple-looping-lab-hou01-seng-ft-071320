832cf5b670d2c5722dfdfddf2ee404edb29fd97c
megan <meggsnbacon777@gmail.com>
2020-07-03 05:35:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,4 +5,10 @@ def loop_message_five_times(message)
     puts message
     puts message
   end 
-
+def loop_message_n_times(message,integer)
+counter = 0 
+  while counter< message.length do
+    put message[counter]
+    counter += 1
+end
+end
